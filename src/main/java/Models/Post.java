515e45e4203403234f090de1f008d568f8c3e819
Beth Hansen and Nicholas Raethke <beth@>
2017-08-07 16:40:17 -0700

    set-up blog project with layout, app, post, and test docs.

---
+++ src/main/java/Models/Post.java
@@ -0,0 +1,14 @@
+package Models;
+
+
+public class Post {
+    private String content;
+
+    public Post (String content) {
+
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
